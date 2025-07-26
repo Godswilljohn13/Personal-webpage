9fe449e3c1a3e7aec62680b61bb89d3e7218e900
Godswilljohn13 <godswilljohnu@gmail.com>
2025-07-26 11:01:03 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+student = {
+"Alice" : [85, 90, 78]
+"Bob": [70, 88, 82]
+"Samson": [95, 100, 95]
+
+}
+subject = ["math","enlish",]
+for name, scores in student.items():
+    print(f"\nReport card for {name}")
+    total = 0
+    for i in range(len[subject]):
+    
+        print(f"{subject[i]}): (scores[i]")
+        total += scores[i]
+    
+        average = total/ len (subjects)
+        print()
